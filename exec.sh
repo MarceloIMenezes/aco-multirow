ae5f9a56c39f665fffc4251a2069a93a210c8147
Marcelo Menezes <marceloian5@gmail.com>
2022-08-17 03:38:31 -0300

    feat: exec com funcao

--- exec.sh
+++ exec.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-FILENAME=N25_01
-M=_4
+auto_exec() {
 
-echo "Criando diretório . . ."
-mkdir ./solution/$FILENAME$M
+    FILENAME=$1
+    M=$2
+    TABLE=$3
 
-for i in $(seq 1 30); do
-    echo "Executando $i"
-    ./main ./data/sfmrflp_instances/table_10/$FILENAME.txt ./solution/$FILENAME$M/exec$i.txt $(($i*4576 + 15753))
-done
+    echo "----- INICIANDO $FILENAME -----"
+    echo "Criando diretório . . ."
+    mkdir ./solution/$FILENAME$M
 
-echo "Passando para CSV . . ."
-python3 ./tocsv.py solution/$FILENAME$M/
-echo "Done"
+    for i in $(seq 1 30); do
+        echo "Executando $i"
+        ./main ./data/sfmrflp_instances/table_$TABLE/$FILENAME.txt ./solution/$FILENAME$M/exec$i.txt $(($i*4576 + 15753))
+    done
+
+    echo "Passando para CSV . . ."
+    python3 ./tocsv.py solution/$FILENAME$M/
+    echo "Done"
+
+}
+
+make
+
+auto_exec AKV_60_05 _5 13
+auto_exec AKV_70_05 _5 13
+auto_exec CAP_n60_p30_s30_1 _5 14
+auto_exec CAP_n60_p90_s60_1 _5 14
